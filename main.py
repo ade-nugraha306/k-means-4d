f6c1a778b32fc00bf988ea7ec6b07cc148aea415
Ade Nugraha <adenuge12@gmail.com>
2025-08-26 16:41:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import numpy as np
 
 def baca_tabel_per_kolom(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
@@ -27,6 +30,7 @@ def baca_tabel_per_kolom(file_path):
     return kolom
 
 
+# Main 
 file_txt = "./dataset.txt"
 hasil = baca_tabel_per_kolom(file_txt)
 
@@ -38,31 +42,78 @@ print("Tinggi Badan: ", hasil["TinggiBadan"])
 print("Lingkar Pinggang: ", hasil["LingkarPinggang"])
 print("Tekanan Darah: ", hasil["TekananDarah"])
 
+# Scatter Plot: Berat Badan vs Lingkar Pinggang
 x = hasil["BeratBadan"]
-y = hasil["LingkarPinggang"]
+y = hasil["TinggiBadan"]
+z = hasil["LingkarPinggang"]
 
-plt.scatter(x, y)
-plt.xlabel("Berat Badan (kg)")
-plt.ylabel("Lingkar Pinggang (cm)")
-plt.title("Scatter Plot Berat Badan vs Lingkar Pinggang")
+# Buat figure dan axis 3D
+fig = plt.figure(figsize=(8,6))
+ax = fig.add_subplot(111, projection='3d')
+
+# Scatter 3D
+ax.scatter(x, y, z)
+
+# Label axis
+ax.set_xlabel("Berat Badan (kg)")
+ax.set_ylabel("Tinggi Badan (cm)")
+ax.set_zlabel("Lingkar Pinggang (cm)")
+
+plt.title("Scatter Plot Berat Badan vs Tinggi Badan vs Lingkar Pinggang")
 plt.show()
 
-data = list(zip(x, y))
+data = np.array(list(zip(x, y, z)))  # pastikan data array
+
+silhouette_scores = []
+
+# range cluster minimal 2
+for i in range(2, 10):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(data)
+    labels = kmeans.labels_
+    score = silhouette_score(data, labels)
+    silhouette_scores.append(score)
+
+plt.plot(range(2, 10), silhouette_scores, marker='o')
+plt.title('Silhouette Score Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('Silhouette Score')
+plt.show()
+
 inertias = []
 
-for i in range(1,10):
+for i in range(1, 10):
     kmeans = KMeans(n_clusters=i)
     kmeans.fit(data)
     inertias.append(kmeans.inertia_)
 
-plt.plot(range(1,10), inertias, marker='o')
-plt.title('Elbow method')
-plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
+plt.plot(range(1,10), inertias, marker='o') 
+plt.title('Elbow method') 
+plt.xlabel('Number of clusters') 
+plt.ylabel('Inertia') 
 plt.show()
+
+# Ambil 3 kolom
+x = hasil["BeratBadan"]
+y = hasil["TinggiBadan"]
+z = hasil["LingkarPinggang"]
+
+# Bikin array 3D
+data = np.array(list(zip(x, y, z)))
 
 kmeans = KMeans(n_clusters=4)
 kmeans.fit(data)
 
-plt.scatter(x, y, c=kmeans.labels_)
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=labels, cmap='viridis', s=50)
+ax.scatter(centroids[:, 0], centroids[:, 1], centroids[:, 2], c='red', s=200, marker='X')
+
+ax.set_xlabel('Berat Badan')
+ax.set_ylabel('Tinggi Badan')
+ax.set_zlabel('Lingkar Pinggang')
 plt.show()
